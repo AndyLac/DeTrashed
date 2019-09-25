4f965f90ffca838875ce061524028f29c0781ca9
Andy Lac <alac@eighty8.co>
2019-09-25 15:58:29 -0400

    Fixed up the Nav Bar logo

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,8 +7,8 @@ const NavBar = () => (
         <ul className="container">
             <li className="logo-container">
                 <ul className="logo-box">
-                    <li className="logo-picture">
-                        <h1 className="logo-placeholder">Test</h1>
+                    <li className="logo">
+                        <h1 className="logo-placeholder">DeTrashed</h1>
                     </li>
                 </ul>
             </li>
